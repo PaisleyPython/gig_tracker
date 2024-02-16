294c91f1b93c239d55c2b2b9806dcb37dfc0ac6a
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2024-02-16 21:07:26 +0000

    initialise sql database and create admin user

--- gig_tracker/settings.py
+++ gig_tracker/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
