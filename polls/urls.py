3c88056da075432fdfa9f22a3199c72f9cf98019
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2024-02-29 16:40:16 +0000

    "Create calander view and update nav bar"

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
@@ -7,7 +7,7 @@ app_name = "polls"
 urlpatterns = [
     path("", views.home),
     path("polls/", views.IndexView.as_view(), name="index"),
-    path("calendar/", views.IndexView.as_view(), name="calendar"),
+    path("calendar/", views.calendar, name="calendar"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
