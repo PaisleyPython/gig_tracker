d5cd1ddc6b79d5e5889234af1f932ecdb064aa82
Simon Jones <148840962+PaisleyPython@users.noreply.github.com>
2024-03-12 16:27:37 +0000

    Update settings.py

--- gig_tracker/settings.py
+++ gig_tracker/settings.py
@@ -114,7 +114,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mail.discopanther@gmail.com'
-EMAIL_HOST_PASSWORD = 'endb fnna kpuu atah'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
 
 
 # Static files (CSS, JavaScript, Images)
